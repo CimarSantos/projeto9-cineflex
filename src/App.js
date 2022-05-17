42b5d7eede5166353b2ffadd851b8f3b1f526602
CimarSantos <francimarlsantos@outlook.com>
2022-05-17 20:37:10 -0300

    Projeto Base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Topo from "./components/Topo.js";
+import Init from "./components/Init.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./assets/styles/reset.css";
+import "./assets/styles/style.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Topo />
+      <Routes>
+        <Route path="/" element={<Init />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/components/Topo.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/styles/style.css";
+
+const Topo = () => {
+  return (
+    <header className="topo flex">
+      <h1>CINEFLEX</h1>
+    </header>
+  );
+};
+
+export default Topo;
