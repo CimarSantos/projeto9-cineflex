52661b50eb208a8cc2ef1fc55a2326e6c98e5dfd
CimarSantos <francimarlsantos@outlook.com>
2022-05-23 17:36:38 -0300

    Tela de Seleção de Assentos e formulário de Reserva

--- src/components/Assentos.js
+++ src/components/Assentos.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -12,6 +12,7 @@ const Boxtitle = styled.div`
     font-size: 24px;
     font-family: "Roboto";
     color: #293845;
+    margin-right: 50px;
   }
 `;
 
@@ -23,12 +24,200 @@ const Container = styled.div`
   flex-direction: column;
   flex-wrap: wrap;
   padding-bottom: 30px;
-  padding-left: 40px;
+  padding-left: 30px;
 `;
 
-export default function Assentos() {
+const Boxfooter = styled.div`
+  background-color: #9eadba;
+  width: 100%;
+  height: 117px;
+  box-shadow: 0 0 5px #000;
+  display: fixed;
+  bottom: 0;
+  align-items: center;
+`;
+
+const Imagebox = styled.div`
+  background-color: #fff;
+  width: 64px;
+  height: 89px;
+  border-radius: 5px;
+  margin: 0 14px;
+
+  img {
+    width: 49px;
+    height: 72px;
+  }
+`;
+const Titulo = styled.h2`
+  font-family: "Roboto";
+  font-size: 26px;
+  color: #293845;
+`;
+
+const Cadeiras = styled.div`
+  gap: 10px;
+  margin-right: 40px;
+  padding: 0 10%;
+`;
+
+const Legenda = styled.div`
+  justify-content: space-evenly;
+  margin-right: 50px;
+
+  div {
+    width: 26px;
+    height: 26px;
+    border-radius: 50%;
+    font-size: 11px;
+    font-family: "Roboto";
+  }
+
+  div .selecionado {
+    background-color: #8dd7cf;
+    border: 1px solid #1aae9e;
+  }
+
+  div .disponivel {
+    background-color: #c3cfd9;
+    border: 1px solid #7b8b99;
+  }
+
+  div .ocupado {
+    background-color: #fbe192;
+    border: 1px solid #f7c52b;
+  }
+
+  p {
+    font-size: 13px;
+    font-family: "Roboto";
+    padding-top: 5px;
+  }
+`;
+
+const Formulario = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    margin-bottom: 20px 0;
+    display: flex;
+  }
+
+  label {
+    font-family: "Roboto";
+    font-size: 18px;
+    color: #293845;
+    padding: 10px;
+    display: inline;
+    margin-top: 10px;
+  }
+
+  input {
+    width: 290px;
+    height: 51px;
+    border-radius: 3px;
+    border: 1px solid #d5d5d5;
+    font-size: 18px;
+    font-family: "Roboto";
+    padding: 0 20px;
+  }
+
+  button {
+    width: 225px;
+    height: 42px;
+    background-color: #e8833a;
+    color: #fff;
+    border: none;
+    border-radius: 3px;
+    font-size: 18px;
+    margin-top: 30px;
+  }
+`;
+
+function Seat({ seatNumber, isAvailable, reserves, setReserves, id }) {
+  const [escolha, setEscolha] = useState("");
+  let newSeat = { seatNumber: seatNumber, id: id };
+
+  function select() {
+    if (escolha === "") {
+      setEscolha("escolha");
+      setReserves([...reserves, newSeat]);
+    } else {
+      for (let i = 0; i < reserves.length; i++) {
+        if (reserves[i].seatNumber === seatNumber) {
+          reserves.splice(i, 1);
+          break;
+        }
+      }
+      setEscolha("");
+    }
+  }
+
+  function ocupado() {
+    alert("Esse assento não está disponível");
+  }
+
+  if (isAvailable === true) {
+    return (
+      <>
+        {seatNumber < 10 ? (
+          <div className={`seat ${escolha}`} onClick={select}>
+            <p>0{seatNumber}</p>
+          </div>
+        ) : (
+          <div className={`seat ${escolha}`} onClick={select}>
+            <p>{seatNumber}</p>
+          </div>
+        )}
+      </>
+    );
+  } else {
+    return (
+      <>
+        {seatNumber < 10 ? (
+          <div className={`seat ocupado`} onClick={ocupado}>
+            <p>0{seatNumber}</p>
+          </div>
+        ) : (
+          <div className={`seat ocupado`} onClick={ocupado}>
+            <p>{seatNumber}</p>
+          </div>
+        )}
+      </>
+    );
+  }
+}
+
+export default function Assentos({
+  reserves,
+  setReserves,
+  name,
+  setName,
+  cpf,
+  setCpf,
+  setFinalId,
+}) {
   const { idSessao } = useParams();
-  const [seat, setSeat] = useState([]);
+  const [seat, setSeat] = useState(null);
+  const [imgFooter, setimgFooter] = useState([]);
+  const [titleFooter, setitleFooter] = useState([]);
+  const [diaFilme, setDiaFilme] = useState([]);
+  const [horaFilme, setHoraFilme] = useState([]);
+  const [assentoSelecionado, setAssentoSelecionado] = useState([]);
+
+  console.log(assentoSelecionado);
+  let navega = useNavigate();
+
+  const mudaRota = (e) => {
+    e.preventDefault();
+    const maxCPFLength = 11;
+    if (name && cpf.length === maxCPFLength) {
+      let path = `/sucesso`;
+      navega(path);
+    }
+  };
 
   useEffect(() => {
     const request = axios.get(
@@ -38,20 +227,157 @@ export default function Assentos() {
     request
       .then((response) => {
         setSeat(response.data.seats);
+        setimgFooter(response.data.movie.posterURL);
+        setitleFooter(response.data.movie.title);
+        setDiaFilme(response.data.day.weekday);
+        setHoraFilme(response.data.name);
       })
       .catch("Aguarde, carregando...");
   }, [idSessao]);
 
+  function validaCpf(e) {
+    if (cpf.length < 11 || e.nativeEvent.inputType === "deleteContentBackward")
+      setCpf(e.target.value);
+  }
+  if (seat === null) {
+    return <p>Carregando</p>;
+  }
   return (
     <>
       <Container>
         <Boxtitle className="flex">
           <h2>Selecione o(os) assento(os)</h2>
         </Boxtitle>
+        {seat ? (
+          <Cadeiras className="flex">
+            {seat.map((assento, index) => (
+              <Cadeira
+                number={assento.name}
+                id={assento.id}
+                isAvailable={assento.isAvailable}
+                assentoSelecionado={assentoSelecionado}
+                setAssentoSelecionado={setAssentoSelecionado}
+              />
+            ))}
+          </Cadeiras>
+        ) : (
+          <p>Carregando</p>
+        )}
+
+        <Legenda className="flex">
+          <div className="flex">
+            <div className="selecionado"></div>
+            <p>Selecionado</p>
+          </div>
+          <div className="flex disponivel">
+            <div className="disponivel"></div>
+            <p>Disponível</p>
+          </div>
+          <div className="flex indisponivel">
+            <div className="ocupado"></div>
+            <p>Indisponível</p>
+          </div>
+        </Legenda>
       </Container>
-      {seat.map((assento, index) => (
-        <div key={index}>{assento.name}</div>
-      ))}
+      <Container>
+        <Formulario>
+          <form className="userInfo" action="">
+            <div>
+              <label htmlFor="nome">Nome do comprador:</label>
+            </div>
+            <div>
+              <input
+                id="nome"
+                className="reservationIn"
+                type="text"
+                placeholder="Qual seu nome?"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label htmlFor="cpf">CPF do comprador:</label>
+            </div>
+            <div>
+              <input
+                id="cpf"
+                className="reservationIn"
+                type="number"
+                placeholder="Qual o número do seu CPF?"
+                value={cpf}
+                onChange={(e) => validaCpf(e)}
+                required
+              />
+            </div>
+            <div>
+              <button type="submit" className="reserve" onClick={mudaRota}>
+                <h3>Reservar assento(s)</h3>
+              </button>
+            </div>
+          </form>
+        </Formulario>
+      </Container>
+      <Boxfooter className="flex">
+        <Imagebox className="flex">
+          <img src={imgFooter} alt="posterMovie"></img>
+        </Imagebox>
+        <Titulo>
+          {titleFooter}
+          <p>
+            {diaFilme} - {horaFilme}
+          </p>
+        </Titulo>
+      </Boxfooter>
     </>
   );
 }
+
+function Cadeira({
+  number,
+  id,
+  isAvailable,
+  assentoSelecionado,
+  setAssentoSelecionado,
+}) {
+  const [selected, setSelected] = useState(false);
+  console.log(selected);
+
+  function select(isAvailable, id) {
+    if (isAvailable) {
+      setSelected(!selected);
+      if (selected) {
+        let array = assentoSelecionado.filter((assento) => assento !== id);
+        setAssentoSelecionado(array);
+      } else {
+        setAssentoSelecionado([...assentoSelecionado, id]);
+      }
+    } else {
+      alert("Assento não disponível...");
+    }
+  }
+
+  return (
+    <Cadacadeira
+      onClick={() => select(isAvailable, id)}
+      selected={selected}
+      isAvailable={isAvailable}
+    >
+      {number}
+    </Cadacadeira>
+  );
+}
+const Cadacadeira = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 26px;
+  height: 26px;
+  background-color: ${(props) =>
+    props.selected ? "#8dd7cf" : props.isAvailable ? "#c3cfd9" : "#fbe192"};
+  border: 1px solid #808f9d;
+  border-radius: 50%;
+  font-size: 11px;
+  font-family: "Roboto";
+`;
+
--- src/components/Sucesso.js
+++ src/components/Sucesso.js
@@ -1,3 +1,126 @@
-export default function Sucesso() {
-  return <h1>Sucesso</h1>;
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+const Boxtitle = styled.div`
+  background-color: #e5e5e5;
+  height: 110px;
+  padding-top: 20px;
+
+  h2 {
+    font-size: 24px;
+    font-family: "Roboto";
+    color: #293845;
+  }
+`;
+
+const Container = styled.div`
+  background-color: #e5e5e5;
+  display: flex;
+  justify-content: center;
+  gap: 40px;
+  flex-direction: column;
+  flex-wrap: wrap;
+  padding-bottom: 30px;
+  padding-left: 40px;
+`;
+
+const Section = styled.div`
+  h3 {
+    font-size: 20px;
+  }
+
+  button {
+    width: 83px;
+    height: 43px;
+    background-color: #e8833a;
+    color: #fff;
+    border: none;
+    border-radius: 3px;
+    font-size: 18px;
+    margin: 25px 10px 0 0;
+  }
+`;
+
+export default function Sucesso({
+  reserves,
+  name,
+  cpf,
+  finalId,
+  setReserves,
+  setCpf,
+  setName,
+}) {
+  const [seat, setSeat] = useState({ name: "", movie: "", day: "" });
+
+  useEffect(() => {
+    let novaReserva = { ids: [], name: name, cpf: cpf };
+    for (let i = 0; i < reserves.length; i++) {
+      novaReserva.ids.push(reserves[i].id);
+    }
+    axios.post(
+      "https://mock-api.driven.com.br/api/v5/cineflex/seats/book-many",
+      novaReserva
+    );
+    let promise = axios.get(
+      `https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${finalId}/seats`
+    );
+    promise.then((info) => setSeat(info.data));
+  }, []);
+
+  function novoCpf() {
+    let aux = "";
+    for (let i = 0; i < cpf.length - 2; i++) {
+      if (i % 3 === 0 && i !== 0) {
+        aux += `${cpf[i]}`;
+      } else {
+        aux += `.${cpf[i]}`;
+      }
+    }
+    aux += "-" + cpf[cpf.length - 2] + cpf[cpf.length - 1];
+    return aux;
+  }
+
+  let stringCPF = novoCpf();
+  let navigate = useNavigate();
+
+  function voltaInit() {
+    setReserves([]);
+    setName("");
+    setCpf("");
+    let path = `/`;
+    navigate(path);
+  }
+
+  return (
+    <>
+      <Container>
+        <Boxtitle className="flex">
+          <h2>Pedido feito com Sucesso!</h2>
+        </Boxtitle>
+        <Section className="Sessao">
+          <h3>Filme e sessão</h3>
+          <h4>{seat.movie.title}</h4>
+          <h4>
+            {seat.day.date} {seat.name}
+          </h4>
+        </Section>
+        <Section className="assento">
+          <h3>Ingressos</h3>
+          {reserves.map((info, i) => (
+            <h4 key={i}>Assento {info.seatNumber}</h4>
+          ))}
+        </Section>
+        <Section className="comprador">
+          <h3>Comprador</h3>
+          <h4>Nome: {name}</h4>
+          <h4>CPF: {stringCPF}</h4>
+        </Section>
+        <button className="reserva" onClick={voltaInit}>
+          Voltar pra Home
+        </button>
+      </Container>
+    </>
+  );
 }
